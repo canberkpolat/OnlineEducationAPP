5b592aa1890959b322333fd36f25da584a92f49c
Canberk Polat <hello@canberkpolat.com>
2020-02-24 00:11:50 +0300

    course-category-stream modelleri eklendi

---
+++ OnlineEducationAPP.MvcWebUI/Models/StreamModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineEducationAPP.MvcWebUI.Models
+{
+    public class StreamModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CourseId { get; set; }
+        public string Endpoint { get; set; }
+        public string StreamKey { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime StartTime { get; set; }
+    }
+}
