fc8f62cd47a60b5deb7855f6c53d246e5937fc13
Canberk Polat <hello@canberkpolat.com>
2020-04-27 04:42:04 +0300

    required

--- OnlineEducationAPP.MvcWebUI/Entity/Stream.cs
+++ OnlineEducationAPP.MvcWebUI/Entity/Stream.cs
@@ -2,6 +2,7 @@
 using OnlineEducationAPP.MvcWebUI.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,8 +19,10 @@ namespace OnlineEducationAPP.MvcWebUI.Entity
         public DateTime? StartTime { get; set; }
         public DateTime? EndTime { get; set; }
         public DateTime? UpdateTime { get; set; }
+        [Required]
         public string UserId { get; set; }
         public virtual ApplicationUser User  { get; set; }
+        [Required]
         public int CourseId { get; set; }
         public virtual Course Course { get; set; }
 
