c56685b760222edd902a1fc0bcd996931afdcad8
Canberk Polat <hello@canberkpolat.com>
2020-05-24 04:50:47 +0300

    calendar event entity model

---
+++ OnlineEducationAPP.MvcWebUI/Entity/Event.cs
@@ -0,0 +1,24 @@
+﻿using OnlineEducationAPP.MvcWebUI.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineEducationAPP.MvcWebUI.Entity
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public DateTime DateBegin { get; set; }
+        public DateTime DateEnd { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public int CourseId { get; set; }
+        public virtual Course Course { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
