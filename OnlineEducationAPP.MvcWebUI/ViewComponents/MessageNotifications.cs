391af41df6e9078aba8c60f31f43a120e87c3472
gumushaydar <gumuskubi@hotmail.com>
2020-05-26 17:49:30 +0300

    message notification test

---
+++ OnlineEducationAPP.MvcWebUI/ViewComponents/MessageNotifications.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEducationAPP.MvcWebUI.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineEducationAPP.MvcWebUI.ViewComponents
+{
+    public class MessageNotifications : ViewComponent
+    {
+
+        private IUnitOfWork unitOfWork;
+
+        public MessageNotifications(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            var messages = unitOfWork.Messages.Find(t => t.ReceiveTime == null).OrderByDescending(t=>t.SendTime).GroupBy(t => t.SenderUser).ToList();
+
+            return View(messages);
+        }
+    }
+}
