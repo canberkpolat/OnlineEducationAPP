e19656dc3bb04b48f1ba95ea8ba2845cf64c402b
gumushaydar <gumuskubi@hotmail.com>
2020-06-19 15:09:24 +0300

    fix

--- OnlineEducationAPP.MvcWebUI/Entity/Category.cs
+++ OnlineEducationAPP.MvcWebUI/Entity/Category.cs
@@ -10,7 +10,7 @@ namespace OnlineEducationAPP.MvcWebUI.Entity
         public int Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
-
+         
         public virtual ICollection<Course> Courses { get; set; }
     }
 }
